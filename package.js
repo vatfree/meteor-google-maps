675d05405f91a3c24f4816755c0bb0e070932f70
Jaco Swarts <jaco@vatfree.com>
2024-07-25 21:14:01 +0200

    Update the googlemaps package

--- package.js
+++ package.js
@@ -1,24 +1,19 @@
 Package.describe({
-  name: 'dburles:google-maps',
-  summary: 'Google Maps Javascript API v3',
-  version: '1.1.5',
-  git: 'https://github.com/dburles/meteor-google-maps.git'
+  name: "dburles:google-maps",
+  summary: "Google Maps Javascript API v3",
+  version: "1.1.5",
+  git: "https://github.com/dburles/meteor-google-maps.git",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use([
-    'templating',
-    'reactive-var',
-    'underscore']);
-  api.addFiles([
-    'google-maps.html',
-    'google-maps.js'], 'client');
-  api.export('GoogleMaps', 'client');
+Package.onUse(function (api) {
+  api.versionsFrom("1.0");
+  api.use(["templating", "reactive-var", "underscore"]);
+  api.addFiles(["google-maps.html", "google-maps.js"], "client");
+  api.export("GoogleMaps", "client");
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('dburles:google-maps');
-  api.addFiles('google-maps-tests.js');
+Package.onTest(function (api) {
+  api.use("tinytest");
+  api.use("dburles:google-maps");
+  api.addFiles("google-maps-tests.js");
 });
